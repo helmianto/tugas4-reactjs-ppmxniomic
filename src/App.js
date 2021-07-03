2d59c7ef1684f180ae68647133f24d0d8add22b9
Helmianto <helmianto15@gmail.com>
2021-07-03 23:14:42 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MenuMakanan from './Page/MenuMakanan';
+
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <MenuMakanan />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/ListData/ListMakanan.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class ListMakanan extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            datalist: this.props.imgsrc
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                <img src={this.state.datalist} alt="Product Makanan" width="150" />
+            </div>
+        );
+    }
+}
+
+export default ListMakanan;
